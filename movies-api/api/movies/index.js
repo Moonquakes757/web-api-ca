43458e33d3d9ad97cf6b7c52a55c56acc58d9539
Moonquakes757 <xsh20020902@gmail.com>
2024-12-30 00:10:56 +0800

    Added two new API endpoints

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,8 +3,41 @@ import asyncHandler from 'express-async-handler';
 import express from 'express';
 import { getUpcomingMovies } from '../tmdb-api';
 import { getMovieGenres } from '../tmdb-api';
+import axios from 'axios';
 
 const router = express.Router();
+
+
+//New router1: GET /api/movies/all
+router.get('/all', asyncHandler(async (req, res) => {
+    try {
+        const movies = await movieModel.find({});
+        return res.status(200).json(movies);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to fetch all movies from DB' });
+    }
+}));
+
+
+//New router2: GET /api/movies/tmdb/:movieId
+router.get('/tmdb/:movieId', asyncHandler(async (req, res) => {
+    try {
+        const { movieId } = req.params;
+        const tmdbKey = process.env.TMDB_KEY;
+        if (!tmdbKey) {
+            return res.status(500).json({ error: 'TMDB_KEY is not set in .env' });
+        }
+
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${tmdbKey}`;
+        const response = await axios.get(url);
+
+        return res.status(200).json(response.data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to fetch movie from TMDB' });
+    }
+}));
 
 router.get('/', asyncHandler(async (req, res) => {
     let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
@@ -35,7 +68,7 @@ router.get('/:id', asyncHandler(async (req, res) => {
     if (movie) {
         res.status(200).json(movie);
     } else {
-        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+        res.status(404).json({ message: 'The movie you requested could not be found.', status_code: 404 });
     }
 }));
 
--- movies-api/index.js
+++ movies-api/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
-app.use('/api/movies', authenticate,moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
